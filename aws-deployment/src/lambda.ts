666fdd9e626097e825d7c0581448163304452d82
Aaron Shiel <ashiel@coldwasp.local>
2025-06-24 12:17:19 -0700

    upgrade serverless-http

--- aws-deployment/src/lambda.ts
+++ aws-deployment/src/lambda.ts
@@ -5,7 +5,7 @@ Permission to use, copy, modify, and distribute this software and its documentat
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 require("source-map-support/register");
-import serverlessExpress from "@vendia/serverless-express";
+import serverlessExpress from "@codegenie/serverless-express";
 import { createApp, appStart } from "abe-gql-core";
 
 // eslint-disable-next-line   @typescript-eslint/no-explicit-any
