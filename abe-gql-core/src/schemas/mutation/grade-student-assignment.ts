672d984f699a0d4bd3e5eb6124105f9b8df752e3
Aaron Shiel <ashiel@coldwasp.local>
2025-08-27 11:28:22 -0700

    unusued car

--- abe-gql-core/src/schemas/mutation/grade-student-assignment.ts
+++ abe-gql-core/src/schemas/mutation/grade-student-assignment.ts
@@ -4,7 +4,7 @@ Permission to use, copy, modify, and distribute this software and its documentat
 
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
-import UserModel, { EducationalRole, UserRole } from "../models/User";
+import UserModel, { EducationalRole } from "../models/User";
 import StudentDataModel, { StudentDataType } from "../models/StudentData";
 import { GraphQLInt, GraphQLNonNull, GraphQLString } from "graphql";
 import * as dotenv from "dotenv";
